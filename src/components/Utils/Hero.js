e2308d77295e8d5596b2d957cc0210fbe9ceb42a
zottwickel <scott.g.mckell@gmail.com>
2020-06-06 20:08:21 -0600

    edge compatability

--- src/components/Utils/Hero.js
+++ src/components/Utils/Hero.js
@@ -22,7 +22,7 @@ const HeroWrapper = styled.header`
   justify-content: center;
   align-items: center;
   .banner {
-    background-color: rgb(189, 189, 189, 0.8);
+    background: rgb(189, 189, 189, 0.8);
     color: black;
     padding: 2rem;
     text-align: center;
